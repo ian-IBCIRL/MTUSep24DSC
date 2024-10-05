c3d7c932c07a54eaa3092c0baa8cf2dbae0d7e61
ian-IBCIRL <ian@ibcirl.com>
2024-10-05 18:29:48 +0000

    Added File Handling Lab demo and Map Demo

---
+++ 02. Java Collections Framework/Lab/FileHandling.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileHandling {
+    public static void main(String[] args) {
+        String fileName = "/workspace/MTUSep24DSC/02. Java Collections Framework/Lab/hamlet.txt";
+
+        try {
+            Scanner in = new Scanner(new File(fileName));
+            int lineNumber = 1;
+            while (in.hasNextLine()) {
+                Scanner lineParser = new Scanner(in.nextLine());
+                // Use any characters other than a-z, A-Z, 0-9 as delimiters
+                lineParser.useDelimiter("[^A-Za-z0-9]+");
+                while (lineParser.hasNext()) {
+                    String word = lineParser.next();
+                    System.out.println("''"+ word);
+                }
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ 02. Java Collections Framework/MapDemo.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+   This program demonstrates a map that maps names to colors.
+*/
+public class MapDemo
+{
+   public static void main(String[] args)
+   {      
+      Map<String, Color> favoriteColors = new HashMap<String, Color>();
+      favoriteColors.put("Juliet", Color.BLUE);
+      favoriteColors.put("Romeo", Color.GREEN);
+      favoriteColors.put("Adam", Color.RED);
+      favoriteColors.put("Eve", Color.BLUE);
+      favoriteColors.put("Kay", Color.BLUE);
+      favoriteColors.put("Kay", Color.ORANGE);// modifies value of existing key
+
+      Color kaysFavoriteColor = favoriteColors.get("Kay");
+      System.out.println(kaysFavoriteColor);//expect Color.ORANGE);
+
+      // Print all keys and values in the map
+
+      Set<String> keySet = favoriteColors.keySet();
+      for (String key : keySet)
+      {
+         Color value = favoriteColors.get(key);
+         System.out.println(key + " : " + value);
+      }
+   }
+}

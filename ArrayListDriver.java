92466f4150deab56f8824713a3fdd9352133d629
ian-IBCIRL <ian@ibcirl.com>
2024-10-05 18:07:24 +0000

    updated ArrayListDriver for new data names

--- ArrayListDriver.java
+++ ArrayListDriver.java
@@ -7,8 +7,8 @@ public class ArrayListDriver {
         names.add("Bill"); // adds at end
         names.add("Jill");
         names.add("Paul");
-        names.add("Sue"); // names now has Bill, Jill, Tim, Sue
-        names.add(1,"Rob"); //names now has Bill, Rob, Jill, Tim, Sue
+        names.add("Sue"); // names now has Bill, Jill, Paul, Sue
+        names.add(1,"John"); //names now has Bill, John, Jill, Paul, Sue
         //this requires elements after Bill to be shifted
 
         //output the contents of the list

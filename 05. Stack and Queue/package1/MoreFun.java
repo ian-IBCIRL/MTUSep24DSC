482a9847840d899418e83df415154a6112264f9f
ian-IBCIRL <54858597+ian-IBCIRL@users.noreply.github.com>
2024-10-14 09:22:10 +0000

    added exception

--- 05. Stack and Queue/package1/MoreFun.java
+++ 05. Stack and Queue/package1/MoreFun.java
@@ -3,8 +3,8 @@ package package1;
 public class MoreFun {
     public void haveMoreFun() {
         System.out.println("Having more fun!");
-//        int number = 10/0;  // to generate an exception
-        int number = 10;  // to NOT generate an exception
+        int number = 10/0;  // to generate an exception
+//        int number = 10;  // to NOT generate an exception
         System.out.println("Finishing more fun"+number);
     }
 }

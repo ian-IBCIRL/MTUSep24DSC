c95b7382b34af7c8b36f776d7c217ba764e58f3e
ian-IBCIRL <54858597+ian-IBCIRL@users.noreply.github.com>
2024-10-14 09:23:44 +0000

    reduced n

--- 05. Stack and Queue/package1/Main.java
+++ 05. Stack and Queue/package1/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
-        int n = 55;
+        int n = 5;
         Fun fun = new Fun();
         fun.haveFun();
         System.out.println("Finishing main "+n);
